ae950804143672931fcc30904abb8577e76e8cec
themerrymarlin <dam134@case.edu>
2018-03-06 10:17:58 -0500

    code update, adding slave, need to get stephen on git

--- beofeng_beta/beofeng_beta.ino
+++ beofeng_beta/beofeng_beta.ino
@@ -1,43 +1,90 @@
 #include <Wire.h>
 
-const byte RADIO_ADDR = 1; 
+//Set the address of the RDA chip
+const byte RADIO_ADDR = 0b0101110;//One option is 46. The other is 113 
 
 
 void setup() {
   
   //Setup for debugging serial communication.
   Serial.begin(9600);
+  
   //Join as Master.
   Wire.begin();
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//   put your main code here, to run repeatedly:
 //  setRegister(9,127,1,50);
 //  readRegister(9, 10);
 //  delay(1);
 
+
+  int addr = pollI2cAddr();
+  Serial.print("Address is: ");
+  Serial.println(addr);
+  delay(100*1000);
+
+
 // Change the frequency in increments of one
-   float freq = 409.0;
-   for(int i=0; i < 100; i++){
+   float freq = 409.75;
+//   for(int i=0; i < 100; i++){
       //changeFreq(freq); //change the frequency
       setRegister(RADIO_ADDR, 0x29, B00000000, B00110010);
+      delay(100);
       setRegister(RADIO_ADDR, 0x2a, B00000100, B10110000);
-      delay(1000);//delay one second
-      
-      //freq = freq + 0.5;
-   }
-   
+      delay(100);//delay one second
+//      setRegister(RADIO_ADDR, 0x29, B00000001, B00110010);
+//      setRegister(RADIO_ADDR, 0x2a, B00000100, B10110000); 
+//      delay(1000);     
+//      freq = freq + 0.5;
+//   }
+  
 }
+
+int* longToBitArr(long l){
+   int* arr = malloc(sizeof(l)*8*sizeof(int));
+   for(int i=0; i<(sizeof(l)*8)-1;i++){
+      arr[i]=bitRead(l,i);
+   }
+   Serial.println(sizeof(arr));
+   return arr;
+}
+
+long bitArrToLong(int* arr){
+  
+  long ret=0;
+  //Serial.println(sizeof(arr)*8);
+  for(int i =0; i<sizeof(arr); i++){
+    bitWrite(ret, i, arr[i]);
+  }
+  return ret;
+} //bitArrToLong
+
+void printArr( int* arr){
+  
+  for(int i=0; i<sizeof(arr);i++){
+    Serial.print(arr[i]);
+    //Dont print comma on last line
+    if ( i<sizeof(arr)-1 ){
+      Serial.print(", ");
+    }
+  }
+  Serial.print("\n");
+} //printArr
+
 
 void changeFreq(float frequency){
 
-  int temp_freq = frequency*1000*8;
+  long temp_freq = (frequency*8000);
   //shift our 32 bit to a 29 bit number;
-  temp_freq = temp_freq>>3;  
-  Serial.println(temp_freq);
-  int high_freq = (int)(temp_freq >>16);
+  Serial.println(temp_freq,BIN);
+  int* arr = longToBitArr(temp_freq);
+  printArr(arr);
+  Serial.println(bitArrToLong(arr));
+  Serial.println(sizeof(temp_freq)*8);
+  int high_freq = (temp_freq >>16);
   byte high_freq_high = highByte(high_freq);
   byte high_freq_low = lowByte(high_freq);
 
@@ -47,12 +94,12 @@ void changeFreq(float frequency){
 
   Serial.println( low_freq_high );
   Serial.println( low_freq_low );
-  Serial.println( " ");
+  Serial.println(" ");
 
   setRegister( RADIO_ADDR, 0x29, high_freq_high, high_freq_low );
-  setRegister( RADIO_ADDR, 0x2a, low_freq_high, low_freq_low);
+  setRegister( RADIO_ADDR, 0x2a, low_freq_high, low_freq_low );
   
-}
+} //changeFrequency
 
 
 //Set the register of a i2c device at a specific register_address with specific 8bit address
@@ -119,4 +166,22 @@ byte* readRegister(byte i2c_address, byte register_address){
   return ret_ptr;  
   
 } //readRegister
+
+int pollI2cAddr(){
+  int retAddr = -1;//First loop will add this to be 0
+  int retCond = -1;
+  
+  while(retCond !=0){
+    retAddr=retAddr+1;
+    Wire.beginTransmission(retAddr);
+    delay(100);
+    retCond = Wire.endTransmission();
+    Serial.print("Return Condition: ");
+    Serial.println(retCond);
+    delay(100);
+    
+  }
+  return retAddr;
+
+}
 
