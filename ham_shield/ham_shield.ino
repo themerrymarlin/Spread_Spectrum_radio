838c54ceec2e84a4ef964c76f9ff47a20ed47913
themerrymarlin <dam134@case.edu>
2018-03-22 10:02:30 -0400

    start of setup

--- ham_shield/ham_shield.ino
+++ ham_shield/ham_shield.ino
@@ -1,9 +1,28 @@
 #include <Wire.h>
 #include <HamShield.h>
 
+HamShield radio;
+
+#define LED_PIN 13
+#define REPORT_RATE 5000
+#define PWM_PIN 3
+#define RESET_PIN A3
+#define SWITCH_PIN 2
+
+uint32_t freq;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(PWM_PIN,OUTPUT);
+  digitalWrite(PWM_PIN,LOW);
 
+  pinMode(SWITCH_PIN,INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+  while(digitalRead(SWITCH_PIN));
+
+  
 }
 
 void loop() {
