005a5772ed2a937c7b5925a086b907b11b7aabe4
avexs <stephenalexmarquis@gmail.com>
2018-03-22 14:06:51 -0400

    added waitforactivity
    Added the wait for activity function. Still bugs on getting stuck
    waiting for activity.
    Reallly rough code.

--- HandyTalkie/HandyTalkie.ino
+++ HandyTalkie/HandyTalkie.ino
@@ -22,7 +22,7 @@
 HamShield radio;
 
 #define LED_PIN 13
-#define RSSI_REPORT_RATE_MS 5000
+#define RSSI_REPORT_RATE_MS 100
 
 //TODO: move these into library
 #define PWM_PIN 3
@@ -107,7 +107,61 @@ void setup() {
 
 }
 
+
+
+
+
+/* 
+  -- waitForActivity (based on HamShield::waitForChannel)
+  - Wait until there is activity on the channel for a specific period of time.
+  - Includes the option to wait for a period of activity followed by a specified period of inactivity
+
+  timeout - (default 0~forever) how long to wait on an empty channel until activity (in mS) 
+  activitywindow - (default 0) the length of inactivity on the channel to look for. (in mS)
+  setRSSI - (default HAMSHIELD_EMPTY_CHANNEL_RSSI=> -110) The dBm threshold to consider a channel empty
+
+  NOTE - RSSI fluctuates back up to EMPTY Channel thresholds during voice communications but stays high around -50<->-25 when using a 1750hz tone from the baofeng
+   
+*/
+
+bool waitForActivity(long timeout = 0, long activitywindow = 0, int activityRSSI = -75, int emptyRSSI = HAMSHIELD_EMPTY_CHANNEL_RSSI) { 
+    int16_t rssi = 0;                                                              // Set RSSI to max received signal
+    for(int x = 0; x < 20; x++) { rssi = radio.readRSSI(); }                       // "warm up" to get past RSSI hysteresis     
+    long timer = millis() + timeout;                                              // Setup the timeout value
+    if(timeout == 0) { timer = 4294967295; }                                      // If we want to wait forever, set it to the max millis()    
+    Serial.println( "waitForAciviity:: Going waiting for signal");
+    while(timer > millis()) {                                                     // while our timer is not timed out.
+        rssi = radio.readRSSI();                                                        // Read signal strength
+        if ( rssi > activityRSSI ){
+          Serial.println("waitForActivity:: ActivityDetected on the channel");
+          timer = millis()+activitywindow;
+          while(timer > millis()){
+            rssi = radio.readRSSI();
+            //if the channel goes dead
+            if( rssi <= emptyRSSI ){
+                Serial.println("waitForActivity:: Channel went dead before the end of the activity window");
+                return false; // Timeout, the channel went dead
+            }
+          }
+          Serial.println("waitForActivity:: End of activity window checking that channel is dead");
+          int FUDGE_FACTOR_FOR_ENDING_COMMUNICATION=100;
+          while( millis()-timer < FUDGE_FACTOR_FOR_ENDING_COMMUNICATION ){
+            rssi = radio.readRSSI();
+            if ( rssi <= emptyRSSI ){
+              Serial.println("waitForActivity:: Activity ended within predetermined window");
+              return true;
+            }
+          }
+          Serial.println("waitForActivity:: Activity did not end within the predetermined window");
+          return false; // Timeout, we started activity but the activity is longer than our defined channel useage
+        }
+    }
+    return false;                                                                  // Timed out
+}
+
+
 void loop() {  
+  //Switch between transmit and recieve depending on transmit button
   if (!digitalRead(SWITCH_PIN))
   {
     if (!currently_tx) 
@@ -119,34 +173,34 @@ void loop() {
       Serial.println("Tx");
       //radio.setTxSourceMic();
       //radio.setRfPower(1);
+      
+      //tone(PWM_PIN, 6000, 1000);
+      //delay(1000);
     }
   } else if (currently_tx) {
     radio.setModeReceive();
     currently_tx = false;
     Serial.println("Rx");
   }
-  
-  
-  if (Serial.available()) {
-    if (Serial.peek() == 'r') {
-      Serial.read();
-      digitalWrite(RESET_PIN, LOW);
-      delay(1000);
-      digitalWrite(RESET_PIN, HIGH);
-      radio.initialize(); // initializes automatically for UHF 12.5kHz channel
-    } else {
-      Serial.setTimeout(40);
-      freq = Serial.parseInt();
-      Serial.flush();
-      radio.frequency(freq);
-      Serial.print("set frequency: ");
-      Serial.println(freq);
-    }
+
+ /* if( cur_freq == 435000 ){
+    cur_freq = 430000;
+  }else{
+    cur_freq = cur_freq + 500;  
   }
   
-  if (!currently_tx && (millis() - rssi_timeout) > RSSI_REPORT_RATE_MS)
-  {
-    Serial.println(radio.readRSSI());
-    rssi_timeout = millis();
-  }
+  radio.frequency(cur_freq);
+  Serial.print("Frequency: ");
+  Serial.println(cur_freq);
+  delay(500);
+` */
+  
+  //Serial.println(radio.readRSSI());
+  Serial.println("Waiting For Activity");
+  Serial.println(waitForActivity(0,1000, -75));
+  //delay(100);
+  
 }
+
+
+
